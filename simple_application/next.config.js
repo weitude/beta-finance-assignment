afddb1f17a0fe4aa82011f1eba8e9cf5826dfbb5
weitude <eric.c.w.lin@gmail.com>
2023-05-25 16:33:16 +0800

    add basePath

--- simple_application/next.config.js
+++ simple_application/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const repo = "beta-finance-assignment";
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 const nextConfig = {
   output: "export",
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
 
 module.exports = nextConfig;
