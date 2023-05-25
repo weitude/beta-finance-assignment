458b4f99b493d68108d72b582ffe0228137f8a78
weitude <eric.c.w.lin@gmail.com>
2023-05-25 19:31:44 +0800

    let isProd = true

--- simple_application/next.config.js
+++ simple_application/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 // const isProd = process.env.NEXT_ENV === "production";
-const isProd = false;
+const isProd = true;
 
 const repo = "beta-finance-assignment";
 const basePath = `/${repo}`;
